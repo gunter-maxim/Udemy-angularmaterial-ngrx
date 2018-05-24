3e065295f871a2e56e1e0d3a82b864e45913dfdc
Gunter <gunterm@dss.nyc.gov>
2018-05-24 14:37:51 -0400

    Added productsUpdated as Subject rxjs

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
+import { ProductsService } from '../products/products.service';
 
 @Component({
   selector: 'app-product',
@@ -9,13 +10,14 @@ export class ProductComponent implements OnInit {
   @Input() productName: string;
   @Output() productClicked = new EventEmitter();
   
-    constructor() { }
+    constructor(private productService: ProductsService) { }
 
   ngOnInit() {
   }
 
   onClicked(){
     this.productClicked.emit();
+    this.productService.deleteProduct(this.productName);
   }
 
 }
--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -1,27 +1,45 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { ProductsService } from './products.service'
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent implements OnInit {
+export class ProductsComponent implements OnInit, OnDestroy {
+  ngOnDestroy(): void {
+    this.productsSubscription.unsubscribe();
+  }
   productName = 'A Book';
-  products = ['A book', 'A tree'];
+  products = [];
+  private productsSubscription: Subscription
 
-  constructor() {
+  constructor(private productsService: ProductsService ) {
     // setTimeout(() => {
-    //   this.productName = 'A tree';
+    //   this.products = this.productsService.getProducts();
     // }, 3000);
    }
-   onAddProduct() {
-      this.products.push(this.productName);
+   onAddProduct(form: NgForm) {
+      // this.products.push(this.productName);
+      console.log(form);
+      if(form.valid){
+        // this.products.push(form.value.productName);
+        this.productsService.addProduct(form.value.productName);
+
+        
+      }
    }
    onRemoveProduct(productName: string){
     this.products = this.products.filter(p=> p!== productName)
     console.log("evenhandler", productName );
    }
   ngOnInit() {
+    // this.products = this.productsService.getProducts(); 
+    this.productsSubscription = this.productsService.productsUpdated.subscribe(() => {
+      this.products = this.productsService.getProducts(); 
+      })
   }
 
 }
