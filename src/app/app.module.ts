47e905e4ea48f9b31c026af0e434984fdeaafc3f
Maxim Gunter <gunter.maxim@gmail.com>
2018-05-23 22:47:13 -0400

    Initial Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import { ProductComponent } from './product/product.component';
+import { ProductsComponent } from './products/products.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductComponent,
+    ProductsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/products.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.css']
+})
+export class ProductsComponent implements OnInit {
+  productName = 'A Book';
+  products = ['A book', 'A tree'];
+  constructor() {
+    // setTimeout(() => {
+    //   this.productName = 'A tree';
+    // }, 3000);
+   }
+   onAddProduct() {
+      this.products.push(this.productName);
+   }
+  ngOnInit() {
+  }
+
+}
